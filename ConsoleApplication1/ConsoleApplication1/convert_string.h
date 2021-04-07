7f3a2c79f1a3b477ad01e71163914af9444a0875
komiyamma <komiyama0629@gmail.com>
2021-04-07 16:44:04 +0900

    hm_php の実験

---
+++ ConsoleApplication1/ConsoleApplication1/convert_string.h
@@ -0,0 +1,18 @@
+/* 
+ * Copyright (c) 2017 Akitsugu Komiyama
+ * under the Apache License Version 2.0
+ */
+
+#pragma once
+
+#include <string>
+
+using namespace std;
+
+// utf8Å®utf16Ç÷
+std::wstring utf8_to_utf16(const std::string &text);
+
+// utf16Å®utf8Ç÷
+std::string utf16_to_utf8(const std::wstring &text);
+
+
