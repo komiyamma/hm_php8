28587549dcd27e1d9580cd56d11012794887f206
komiyamma <komiyama0629@gmail.com>
2021-04-21 04:05:49 +0900

    GetCursurPosやGetMousePosなどを追加

--- php-sdk/phpdev/vs16/x86/php-8.0.3-src/ext/hidemaru/hidemaru_arginfo.h
+++ php-sdk/phpdev/vs16/x86/php-8.0.3-src/ext/hidemaru/hidemaru_arginfo.h
@@ -1,8 +1,8 @@
 /* This is a generated file, edit the .stub.php file instead.
  * Stub hash: 7eb3fd4083c98e6dffc8b02b6373b7ce9cbf228d */
 
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_test1, 0, 0, IS_VOID, 0)
-ZEND_END_ARG_INFO()
+// ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_test1, 0, 0, IS_VOID, 0)
+// ZEND_END_ARG_INFO()
 
 // ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_test2, 0, 0, IS_STRING, 0)
 // 	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, str, IS_STRING, 0, "\"\"")
@@ -16,6 +16,12 @@ ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_hidemaru_debuginfo, 0, 0, IS_VOI
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_hidemaru_edit_filepath, 0, 0, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_hidemaru_edit_getcursorpos, 0, 0, IS_ARRAY, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_hidemaru_edit_getcursorposfrommousepos, 0, 0, IS_ARRAY, 0)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_hidemaru_edit_totaltext, 0, 0, IS_STRING, 0)
@@ -44,13 +50,18 @@ ZEND_END_ARG_INFO()
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_hidemaru_outputpane_clear, 0, 0, IS_LONG, 0)
 ZEND_END_ARG_INFO()
 
-ZEND_FUNCTION(test1);
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_hidemaru_outputpane_getwindowhandle, 0, 0, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+// ZEND_FUNCTION(test1);
 // ZEND_FUNCTION(test2);
 
 
 ZEND_FUNCTION(hidemaru_version);
 ZEND_FUNCTION(hidemaru_debuginfo);
 ZEND_FUNCTION(hidemaru_edit_filepath);
+ZEND_FUNCTION(hidemaru_edit_getcursorpos);
+ZEND_FUNCTION(hidemaru_edit_getcursorposfrommousepos);
 ZEND_FUNCTION(hidemaru_edit_totaltext);
 ZEND_FUNCTION(hidemaru_edit_selectedtext);
 ZEND_FUNCTION(hidemaru_edit_linetext);
@@ -59,14 +70,17 @@ ZEND_FUNCTION(hidemaru_outputpane_push);
 ZEND_FUNCTION(hidemaru_outputpane_pop);
 ZEND_FUNCTION(hidemaru_outputpane_sendmessage);
 ZEND_FUNCTION(hidemaru_outputpane_clear);
+ZEND_FUNCTION(hidemaru_outputpane_getwindowhandle);
 
 
 static const zend_function_entry ext_functions[] = {
-	ZEND_FE(test1, arginfo_test1)
+//	ZEND_FE(test1, arginfo_test1)
 //	ZEND_FE(test2, arginfo_test2)
 	ZEND_FE(hidemaru_version, arginfo_hidemaru_version)
 	ZEND_FE(hidemaru_debuginfo, arginfo_hidemaru_debuginfo)
 	ZEND_FE(hidemaru_edit_filepath, arginfo_hidemaru_edit_filepath)
+	ZEND_FE(hidemaru_edit_getcursorpos, arginfo_hidemaru_edit_getcursorpos)
+	ZEND_FE(hidemaru_edit_getcursorposfrommousepos, arginfo_hidemaru_edit_getcursorposfrommousepos)
 	ZEND_FE(hidemaru_edit_totaltext, arginfo_hidemaru_edit_totaltext)
 	ZEND_FE(hidemaru_edit_selectedtext, arginfo_hidemaru_edit_selectedtext)
 	ZEND_FE(hidemaru_edit_linetext, arginfo_hidemaru_edit_linetext)
@@ -75,5 +89,6 @@ static const zend_function_entry ext_functions[] = {
 	ZEND_FE(hidemaru_outputpane_pop, arginfo_hidemaru_outputpane_pop)
 	ZEND_FE(hidemaru_outputpane_sendmessage, arginfo_hidemaru_outputpane_sendmessage)
 	ZEND_FE(hidemaru_outputpane_clear, arginfo_hidemaru_outputpane_clear)
+	ZEND_FE(hidemaru_outputpane_getwindowhandle, arginfo_hidemaru_outputpane_getwindowhandle)
 	ZEND_FE_END
 };
