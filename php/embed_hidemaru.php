16332a7b8540f5be77313b7dff9fa9f74e627e04
komiyamma <komiyama0629@gmail.com>
2021-04-21 14:53:34 +0900

    OutputPane系

--- php/embed_hidemaru.php
+++ php/embed_hidemaru.php
@@ -27,6 +27,16 @@ class _TEdit {
 			"Column" => $pos[1]
 		);
 	}
+
+	public function getMousePos(): array {
+		$pos = hidemaru_edit_getcursorposfrommousepos();
+		return array(
+			"LineNo" => $pos[0],
+			"Column" => $pos[1],
+			"X" => $pos[2],
+			"Y" => $pos[3]
+		);
+	}
 }
 
 class _TMacro {
@@ -48,22 +58,56 @@ class _TMacro {
 	}
 }
 
+class _TOutputPane {
+	function output(string $message): bool {
+		$mod_message = str_replace("\n", "\r\n", $message);
+		$mod_message = str_replace("\r\r", "\r", $mod_message);
+		return hidemaru_outputpane_output($mod_message);
+	}
+
+	function clear(): int {
+		return hidemaru_outputpane_clear();
+	}
+
+	function push(): bool {
+		return hidemaru_outputpane_push();
+	}
+
+	function pop(): bool {
+		return hidemaru_outputpane_pop();
+	}
+
+	function setBaseDir(string $dirpath): bool {
+		return hidemaru_outputpane_setbasedir($dirpath);
+	}
+
+	function sendMessage(int $command_id): int {
+		return hidemaru_outputpane_sendmessage($command_id);
+	}
+
+	function getWindowHandle(): int {
+		return hidemaru_outputpane_getwindowhandle();
+	}
+}
+
 class _THidemaru {
 
 	public $Edit;
 	public $Macro;
+	public $OutputPane;
 
 	function __construct() {
 		$this->Edit = new _TEdit();
 		$this->Macro = new _TMacro();
+		$this->OutputPane = new _TOutputPane();
 	}
 
 	function getVersion(): float {
 		return hidemaru_version();
 	}
 
-	function debugInfo(mixed $message): void {
-		hidemaru_debuginfo(strval($message));
+	function debugInfo(string $message): void {
+		hidemaru_debuginfo($message);
 	}
 
 	function getWindowHandle(): int {
