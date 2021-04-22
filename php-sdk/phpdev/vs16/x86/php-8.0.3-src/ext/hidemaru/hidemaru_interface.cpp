ec40f7fbfd9b516accc305e43c6161c519f4e20d
komiyamma <komiyama0629@gmail.com>
2021-04-22 22:53:39 +0900

    embed_hidemaru.dll のパスを php_hidemaru へと渡す処理

--- php-sdk/phpdev/vs16/x86/php-8.0.3-src/ext/hidemaru/hidemaru_interface.cpp
+++ php-sdk/phpdev/vs16/x86/php-8.0.3-src/ext/hidemaru/hidemaru_interface.cpp
@@ -13,3 +13,9 @@ BOOL isEntryPointIsHidemaru() {
 extern "C" __declspec(dllexport) void set_entry_is_hidemaru() {
 	entry_point_is_hidemaru = TRUE;
 }
+
+wstring embed_hideamru_dll_path = L"";
+extern "C" __declspec(dllexport) void set_embed_hideamru_dll_path(const wchar_t* dllpath) {
+	embed_hideamru_dll_path = wstring(dllpath);
+	MessageBox(NULL, embed_hideamru_dll_path.c_str(), embed_hideamru_dll_path.c_str(), NULL);
+}
