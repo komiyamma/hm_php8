bad609449ab7ad9be9debf060b34a7e0b70aa495
komiyamma <komiyama0629@gmail.com>
2021-04-22 19:45:52 +0900

    ヘッダにまわす

--- HmPHP8/hmPHP8/dllfunc_interface.cpp
+++ HmPHP8/hmPHP8/dllfunc_interface.cpp
@@ -13,16 +13,10 @@
 #include "dllfunc_interface.h"
 #include "dllfunc_interface_internal.h"
 
+#include "embed_hidemaru.h"
 
 using namespace std;
 
-extern BOOL DoCreate();
-extern BOOL DoDestroy();
-extern intHM_t Include(const wchar_t* utf16_expression);
-extern intHM_t PHPGetNumVar(const wchar_t* utf16_simbol);
-extern intHM_t PHPSetNumVar(const wchar_t* utf16_simbol, intHM_t value);
-extern wstring PHPGetStrVar(wstring utf16_simbol);
-extern BOOL PHPSetStrVar(wstring utf16_simbol, wstring utf16_value);
 
 //------------------------------------------------------------------------------------
 // 対象のシンボル名の値を数値として得る
